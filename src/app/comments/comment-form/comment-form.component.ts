1219e51eb86dbe45822daf70d4b2012914d98ef8
Максим Иванов <omaxphp@yandex.ru>
2026-07-25 21:45:18 +0300

    refactor: remove app prefix from comment form

--- src/app/comments/comment-form/comment-form.component.ts
+++ src/app/comments/comment-form/comment-form.component.ts
@@ -2,13 +2,13 @@ import {ChangeDetectionStrategy, Component, inject, input} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {TuiButton, TuiIcon} from '@taiga-ui/core';
 
-import {OnboardingHintStepDirective} from '../../onboarding/onboarding-hint-step.directive';
+import {OnboardingStepDirective} from '../../onboarding/onboarding-step.directive';
 import {ProposalDto} from '../../proposal.dto';
 import {CommentOnboardingService} from '../comment-onboarding/comment-onboarding.service';
 
 @Component({
-    selector: 'app-comment-form',
-    imports: [FormsModule, OnboardingHintStepDirective, TuiButton, TuiIcon],
+    selector: 'comment-form',
+    imports: [FormsModule, OnboardingStepDirective, TuiButton, TuiIcon],
     templateUrl: './comment-form.component.html',
     styleUrl: './comment-form.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
