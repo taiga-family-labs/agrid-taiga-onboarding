85890694aeb005797ef4840bb657ef03e389c072
Максим Иванов <omaxphp@yandex.ru>
2026-07-25 09:43:12 +0300

    fix: use Taiga UI v4 bootstrap setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {bootstrapApplication} from '@angular/platform-browser';
-import {provideTaiga} from '@taiga-ui/core';
 
 import {AppComponent} from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideAnimations(), provideTaiga()],
+    providers: [provideAnimations()],
 }).catch(console.error);
