88e747cbf600828f0b48d4623e20cb7f7fb97a67
Максим Иванов <omaxphp@yandex.ru>
2026-07-25 21:39:30 +0300

    refactor: extract registered onboarding class

---
+++ src/app/onboarding/onboarding.ts
@@ -0,0 +1,44 @@
+import {type PolymorpheusContent} from '@taiga-ui/polymorpheus';
+
+import {OnboardingService} from './onboarding.service';
+import {
+    type OnboardingOptions,
+    type OnboardingSteps,
+} from './onboarding.types';
+
+export class Onboarding<TSteps extends readonly PolymorpheusContent[]> {
+    public readonly id: string;
+    public readonly steps: OnboardingSteps<TSteps>;
+
+    constructor(
+        private readonly service: OnboardingService,
+        options: OnboardingOptions<TSteps>,
+    ) {
+        this.id = options.id;
+        this.steps = options.steps.map((content, index) => ({
+            onboardingId: options.id,
+            index: index + 1,
+            content,
+        })) as OnboardingSteps<TSteps>;
+    }
+
+    public start(force = false): boolean {
+        return this.service.start(this.id, force);
+    }
+
+    public next(): void {
+        this.service.next(this.id);
+    }
+
+    public close(): void {
+        this.service.close(this.id);
+    }
+
+    public shouldAutoStart(): boolean {
+        return this.service.shouldAutoStart(this.id);
+    }
+
+    public unregister(): void {
+        this.service.unregister(this.id);
+    }
+}
