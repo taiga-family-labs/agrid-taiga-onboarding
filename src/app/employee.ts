95ea6136c74e4d336c3ecdcfb5457feb9c6bed55
Максим Иванов <omaxphp@yandex.ru>
2026-07-25 19:38:48 +0300

    refactor: replace employee row model with proposal DTO

--- src/app/employee.ts
+++
@@ -1,9 +0,0 @@
-export interface Employee {
-    readonly id: number;
-    readonly name: string;
-    readonly department: string;
-    readonly currentCr: number;
-    readonly recommendedCr: number;
-    readonly status: string;
-    readonly comments: number;
-}
