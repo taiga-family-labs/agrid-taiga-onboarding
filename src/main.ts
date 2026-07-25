bd541e2ebd258439d3739cb1c19799c6c8ab8605
Максим Иванов <omaxphp@yandex.ru>
2026-07-25 18:05:50 +0300

    fix: register Taiga UI event plugins

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import {provideAnimations} from '@angular/platform-browser/animations';
 import {bootstrapApplication} from '@angular/platform-browser';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideEventPlugins} from '@taiga-ui/event-plugins';
 
 import {AppComponent} from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideAnimations()],
+    providers: [provideAnimations(), provideEventPlugins()],
 }).catch(console.error);
