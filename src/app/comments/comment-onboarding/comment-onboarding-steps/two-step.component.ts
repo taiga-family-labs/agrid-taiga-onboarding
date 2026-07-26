495cbd503cae83e3c0cd6c475dddf30331808a26
Максим Иванов <omaxphp@yandex.ru>
2026-07-26 10:15:15 +0300

    refactor: rely on Taiga projection selectors in second step

--- src/app/comments/comment-onboarding/comment-onboarding-steps/two-step.component.ts
+++ src/app/comments/comment-onboarding/comment-onboarding-steps/two-step.component.ts
@@ -10,7 +10,7 @@ import {OnboardingService} from '../../../onboarding/onboarding.service';
     imports: [OnboardingStepComponent, TuiButton, TuiTitle],
     template: `
         <onboarding-step>
-            <h3 onboardingStepTitle tuiTitle>
+            <h3 tuiTitle>
                 Все комментарии в одном месте
                 <span tuiSubtitle>Комментарии, отклонения и обоснования теперь будут находиться здесь</span>
             </h3>
@@ -30,7 +30,6 @@ import {OnboardingService} from '../../../onboarding/onboarding.service';
             </div>
 
             <button
-                onboardingStepAction
                 appearance="flat"
                 size="s"
                 tuiButton
