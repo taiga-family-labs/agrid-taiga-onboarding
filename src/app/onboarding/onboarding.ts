8646cb32cb06b8b0a0bf911c414e3c39e3384e75
Максим Иванов <omaxphp@yandex.ru>
2026-07-26 19:27:13 +0300

    refactor: simplify single onboarding controller

--- src/app/onboarding/onboarding.ts
+++ src/app/onboarding/onboarding.ts
@@ -1,48 +1,35 @@
 import {type PolymorpheusContent} from '@taiga-ui/polymorpheus';
 
 import {OnboardingService} from './onboarding.service';
-import {
-    type OnboardingOptions,
-    type OnboardingSteps,
-} from './onboarding.types';
+import {type OnboardingSteps} from './onboarding.types';
 
 export class Onboarding<TSteps extends readonly PolymorpheusContent[]> {
-    public readonly id: string;
-    public readonly steps: OnboardingSteps<TSteps>;
-
-    constructor(
+    public constructor(
         private readonly service: OnboardingService,
-        options: OnboardingOptions<TSteps>,
-    ) {
-        this.id = options.id;
-        this.steps = options.steps.map((content, index) => ({
-            onboardingId: options.id,
-            index: index + 1,
-            content,
-        })) as OnboardingSteps<TSteps>;
-    }
+        public readonly steps: OnboardingSteps<TSteps>,
+    ) {}
 
     public start(force = false): boolean {
-        return this.service.start(this.id, force);
+        return this.service.start(force);
     }
 
     public next(): void {
-        this.service.next(this.id);
+        this.service.next();
     }
 
     public close(): void {
-        this.service.close(this.id);
+        this.service.close();
     }
 
     public clearMutedState(): void {
-        this.service.clearMutedState(this.id);
+        this.service.clearMutedState();
     }
 
     public shouldAutoStart(): boolean {
-        return this.service.shouldAutoStart(this.id);
+        return this.service.shouldAutoStart();
     }
 
     public unregister(): void {
-        this.service.unregister(this.id);
+        this.service.unregister();
     }
 }
