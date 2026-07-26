f6a835dff743552bb47336e76cbb841fb5559604
Максим Иванов <omaxphp@yandex.ru>
2026-07-26 20:15:33 +0300

    refactor: provide comments onboarding by feature flag

---
+++ src/app/comments/comment-onboarding/comment-onboarding.provider.ts
@@ -0,0 +1,21 @@
+import {inject, InjectionToken, type Provider} from '@angular/core';
+
+import {APP_CONFIG} from '../../app-config';
+import {CommentOnboardingService} from './comment-onboarding.service';
+
+export const COMMENT_ONBOARDING = new InjectionToken<CommentOnboardingService | null>(
+    '[COMMENT_ONBOARDING]: CommentOnboardingService',
+);
+
+export function provideCommentOnboarding(): Provider[] {
+    return [
+        CommentOnboardingService,
+        {
+            provide: COMMENT_ONBOARDING,
+            useFactory: () =>
+                inject(APP_CONFIG).features?.enableTriggersUiImprovement
+                    ? inject(CommentOnboardingService)
+                    : null,
+        },
+    ];
+}
