d2cfcc8aad4f705a9c405e9234307593c44631a8
Максим Иванов <omaxphp@yandex.ru>
2026-07-26 20:18:46 +0300

    refactor: inject optional onboarding in comments sidebar

--- src/app/comments/comments-sidebar/comments-sidebar.component.ts
+++ src/app/comments/comments-sidebar/comments-sidebar.component.ts
@@ -5,7 +5,7 @@ import {TuiSegmented} from '@taiga-ui/kit';
 import {OnboardingStepDirective} from '../../onboarding/onboarding-step.directive';
 import {ProposalDto} from '../../proposal.dto';
 import {CommentFormComponent} from '../comment-form/comment-form.component';
-import {CommentOnboardingService} from '../comment-onboarding/comment-onboarding.service';
+import {COMMENT_ONBOARDING} from '../comment-onboarding/comment-onboarding.provider';
 
 @Component({
     selector: 'comments-sidebar',
@@ -15,7 +15,7 @@ import {CommentOnboardingService} from '../comment-onboarding/comment-onboarding
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentsSidebarComponent {
-    protected readonly commentOnboarding = inject(CommentOnboardingService);
+    protected readonly commentOnboarding = inject(COMMENT_ONBOARDING);
     protected activeSegment = 0;
 
     public readonly proposal = input.required<ProposalDto>();
